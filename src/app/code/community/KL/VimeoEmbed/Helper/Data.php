f08571d2811f3f007b56f94c18bd8ed494021f78
David Wickström <davidwickstrom@gmail.com>
2015-04-26 09:31:03 +0200

    Acount for Vimeo channels type urls

--- src/app/code/community/KL/VimeoEmbed/Helper/Data.php
+++ src/app/code/community/KL/VimeoEmbed/Helper/Data.php
@@ -6,7 +6,7 @@ class KL_VimeoEmbed_Helper_Data extends Mage_Core_Model_Abstract
     {
         $width = $width ? " width=\"{$width}\"" : '';
         $height = $height ? " height=\"{$height}\"" : '';
-        $template = '<iframe src="https://player.vimeo.com/video{{ id }}"{{ width }}{{ height }} frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+        $template = '<iframe src="https://player.vimeo.com/video/{{ id }}"{{ width }}{{ height }} frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
 
         return strtr($template, [
             '{{ id }}' => $this->parseId($url),
@@ -17,6 +17,7 @@ class KL_VimeoEmbed_Helper_Data extends Mage_Core_Model_Abstract
 
     private function parseId($url)
     {
-        return parse_url($url, PHP_URL_PATH);
+        $path = parse_url($url, PHP_URL_PATH);
+        return end(explode('/', $path));
     }
 }
--- tests/EmbedTest.php
+++ tests/EmbedTest.php
@@ -5,7 +5,6 @@ class EmbedTest extends PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
-//        \Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
         Mage::init();
     }
     
@@ -22,10 +21,20 @@ class EmbedTest extends PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function it_write_the_correct_embed_code_with_width_and_height()
+    public function it_can_write_the_correct_embed_code_with_width_and_height()
     {
         $template = '<iframe src="https://player.vimeo.com/video/61063778" width="500" height="450" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
         $embedCode = Mage::helper('vimeo')->embed('https://vimeo.com/61063778', 500, 450);
+        $this->assertEquals($template, $embedCode);
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_also_deal_with_vimeo_channels_type_urls()
+    {
+        $template = '<iframe src="https://player.vimeo.com/video/120479695" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+        $embedCode = Mage::helper('vimeo')->embed('https://vimeo.com/channels/staffpicks/120479695');
         $this->assertEquals($template, $embedCode);
     }
 }
